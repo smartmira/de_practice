c244b075b7aed371399d8635e8f20d029db73b4d
smartmira <smartmira77@gmail.com>
2024-11-15 03:59:40 -0800

    hyperparameter tuning

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -59,4 +59,4 @@ if __name__=="__main__":
     train_arr,test_arr,_= data_transformation.initiate_data_transformation(train_data,test_data)
 
     model_trainer=ModelTrainer()
-    print(model_trainer.innitiate_model_trainer(train_arr,test_arr))
+    print(model_trainer.initiate_model_trainer(train_arr,test_arr))
